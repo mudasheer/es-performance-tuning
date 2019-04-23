b449bb702ab45b5a7f6680ab39bc57d4bb9e9d28
sathish <sathishkumar.thiyagarajan@gmail.com>
2019-04-23 12:49:44 +0530

    beautify

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "Stopping existing cluster"
 ps -ef | grep 'elasticsearch' | grep -v grep | awk '{print $2}' | xargs kill
 
@@ -8,7 +9,7 @@ rm -rf data_*
 masters=$1
 for (( i=1; i<=$masters; i++ )); 
 do 
-    echo "Starting Dedicated Master#$i" 
+    echo Starting $'\e[1;32m'Dedicated Master$'\e[0m'  $i
     eval "./elasticsearch-7.0.0/bin/elasticsearch -d -Ecluster.name=my_cluster -Enode.data=false -Enode.ingest=false -Ecluster.remote.connect=false -Epath.data=../data_master_$i -Enode.name=master_$i" ; 
 done
 
@@ -16,39 +17,39 @@ done
 data=$2
 for (( i=1; i<=$data; i++ )); 
 do 
-    echo "Starting Data Node#$i" 
+    echo Starting $'\e[1;32m'Data Node$'\e[0m'  $i
     eval "./elasticsearch-7.0.0/bin/elasticsearch -d -Ecluster.name=my_cluster -Enode.master=false -Enode.ingest=false -Ecluster.remote.connect=false -Epath.data=../data_$i -Enode.name=data_$i" ; 
 done
 
 ingest=$3
 for (( i=1; i<=$ingest; i++ )); 
 do 
-    echo "Starting Ingest Node#$i" 
+    echo Starting $'\e[1;32m'Ingest Node$'\e[0m'  $i
     eval "./elasticsearch-7.0.0/bin/elasticsearch -d -Ecluster.name=my_cluster -Enode.master=false -Enode.data=false -Ecluster.remote.connect=false -Epath.data=../data_ingest_$i -Enode.name=ingest_$i" ; 
 done
 
 coordinating=$4
 for (( i=1; i<=$coordinating; i++ )); 
 do 
-    echo "Starting Coordinating Node#$i" 
+    echo Starting $'\e[1;32m'Coordinating Node$'\e[0m'  $i
     eval "./elasticsearch-7.0.0/bin/elasticsearch -d -Ecluster.name=my_cluster -Enode.master=false -Enode.data=false -Enode.ingest=false -Ecluster.remote.connect=false -Epath.data=../data_coordinating_$i -Enode.name=coordinating_$i" ; 
 done
 
-echo "Waiting for Elastic Search to start" 
+echo $'\e[1;34m'Waiting for Elastic Search to start$'\e[0m'
 until $(curl --output /dev/null --silent --head --fail localhost:9200); do
-    printf '.'
+    printf $'\e[1;34m'.$'\e[0m'
     sleep 5
 done
+printf "\n"
 
 for n in $(ls sample/mappings/ | awk '{print $1}' | cut -f 1 -d '.')
 do
-    echo "Uploading Mapping for $n" 
+    echo Uploading Mapping for $'\e[1;34m'$n $'\e[0m'
     eval "curl -H 'Content-Type: application/json' -XPUT 'localhost:9200/${n}?pretty' --data-binary @sample/mappings/${n}.json"
 done
 
 for n in $(ls sample/data/ | awk '{print $1}' | cut -f 1 -d '.')
 do
-    echo "Uploading Data for $n" 
+    echo Uploading Data for $'\e[1;34m'$n $'\e[0m'
     eval "curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/${n}/_doc/_bulk?pretty' --data-binary @sample/data/${n}.json"
 done
-
